55741da454cdcdd11e82df0bce036deedabf062c
bijenkorf-james-wakefield <james.wakefield@debijenkorf.nl>
2020-06-17 20:46:20 +0200

    test(app): update tests for App

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,20 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('<App />', () => {
+
+  test('renders link', () => {
+    const { getByText } = render(<App />);
+    const linkElement = getByText(/my link/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  test('should call click handler passed as handleClick prop', () => {
+    const onClickSpy = jest.fn();
+    const { getByText } = render(<App handleClick={onClickSpy} />);
+    const linkElement = getByText(/my link/i);
+    fireEvent.click(linkElement);
+    expect(onClickSpy).toHaveBeenCalled()
+  });
+})
